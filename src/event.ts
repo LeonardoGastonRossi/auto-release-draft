8ce546cd1421189a8fe42719506f688f33bd7565
Leonardo Rossi <lrossi@solstice.com>
2020-04-10 14:24:13 -0300

    Changes.

--- src/event.ts
+++ src/event.ts
@@ -1,14 +1,13 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { defaultCoreCipherList } from 'constants';
 
 export function getCreatedTag(): string | null {
-    if (github.context.eventName != 'create') {
+    if (github.context.eventName !== 'create') {
         core.info('The event name was ${github.context.eventName}')
         return null;
     }
 
-    if (github.context.payload.ref_type != 'tag') {
+    if (github.context.payload.ref_type !== 'tag') {
         core.info('The created reference was a branch, not a tag')
     }
 
