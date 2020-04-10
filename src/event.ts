fd567443156388f229a7237674ec6ed41befe915
Leonardo Rossi <lrossi@solstice.com>
2020-04-10 14:21:38 -0300

    Checking tag version.

---
+++ src/event.ts
@@ -0,0 +1,16 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { defaultCoreCipherList } from 'constants';
+
+export function getCreatedTag(): string | null {
+    if (github.context.eventName != 'create') {
+        core.info('The event name was ${github.context.eventName}')
+        return null;
+    }
+
+    if (github.context.payload.ref_type != 'tag') {
+        core.info('The created reference was a branch, not a tag')
+    }
+
+    return github.context.payload.ref;
+}
